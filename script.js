df2566559529159136330773986b49b40b5c1a86
ElliotB <mail@elliotbentley.com>
2013-08-11 12:48:18 +0100

    Not going great... sort of works, but checkboxes are not saved

--- script.js
+++ script.js
@@ -1,167 +1,5 @@
-var hiraganaMonographs = {
-  "あ": "A", "い": "I", "う": "U", "え": "E", "お": "O",
-  "か": "KA", "き": "KI", "く": "KU", "け": "KE", "こ": "KO",
-  "さ": "SA", "し": "SHI", "す": "SU", "せ": "SE", "そ": "SO",
-  "た": "TA", "ち": "CHI", "つ": "TSU", "て": "TE", "と": "TO",
-  "な": "NA", "に": "NI", "ぬ": "NU", "ね": "NE", "の": "NO",
-  "は": "HA", "ひ": "HI", "ふ": "FU", "へ": "HE", "ほ": "HO",
-  "ま": "MA", "み": "MI", "む": "MU", "め": "ME", "も": "MO",
-  "や": "YA", "ゆ": "YU", "よ": "YO",
-  "ら": "RA", "り": "RI", "る": "RU", "れ": "RE", "ろ": "RO",
-  "わ": "WA", "ゐ": "I", "ゑ": "E", "を": "O", "ん": "N",
-  "が": "GA", "ぎ": "GI", "ぐ": "GU", "げ": "GE", "ご": "GO",
-  "ざ": "ZA", "じ": "JI", "ず": "ZU", "ぜ": "ZE", "ぞ": "ZO",
-  "だ": "DA", "ぢ": "JI", "づ": "ZU", "で": "DE", "ど": "DO",
-  "ば": "BA", "び": "BI", "ぶ": "BU", "べ": "BE", "ぼ": "BO",
-  "ぱ": "PA", "ぴ": "PI", "ぷ": "PU", "ぺ": "PE", "ぽ": "PO"
-};
 
 var hiragana = [
-    {"term": "あ", "dictionary": "hiragana", "readings": ["a"]},
-    {"term": "い", "dictionary": "hiragana", "readings": ["i"]},
-    {"term": "う", "dictionary": "hiragana", "readings": ["u"]},
-    {"term": "え", "dictionary": "hiragana", "readings": ["e"]},
-    {"term": "お", "dictionary": "hiragana", "readings": ["o"]},
-
-    {"term": "か", "dictionary": "hiragana", "readings": ["ka"]},
-    {"term": "き", "dictionary": "hiragana", "readings": ["ki"]},
-    {"term": "く", "dictionary": "hiragana", "readings": ["ku"]},
-    {"term": "け", "dictionary": "hiragana", "readings": ["ke"]},
-    {"term": "こ", "dictionary": "hiragana", "readings": ["ko"]},
-
-    {"term": "さ", "dictionary": "hiragana", "readings": ["sa"]},
-    {"term": "し", "dictionary": "hiragana", "readings": ["shi", "si"]},
-    {"term": "す", "dictionary": "hiragana", "readings": ["su"]},
-    {"term": "せ", "dictionary": "hiragana", "readings": ["se"]},
-    {"term": "そ", "dictionary": "hiragana", "readings": ["so"]},
-
-    {"term": "た", "dictionary": "hiragana", "readings": ["ta"]},
-    {"term": "ち", "dictionary": "hiragana", "readings": ["chi"]},
-    {"term": "つ", "dictionary": "hiragana", "readings": ["tsu"]},
-    {"term": "て", "dictionary": "hiragana", "readings": ["te"]},
-    {"term": "と", "dictionary": "hiragana", "readings": ["to"]},
-
-    {"term": "な", "dictionary": "hiragana", "readings": ["na"]},
-    {"term": "に", "dictionary": "hiragana", "readings": ["ni"]},
-    {"term": "ぬ", "dictionary": "hiragana", "readings": ["nu"]},
-    {"term": "ね", "dictionary": "hiragana", "readings": ["ne"]},
-    {"term": "の", "dictionary": "hiragana", "readings": ["no"]},
-
-    {"term": "は", "dictionary": "hiragana", "readings": ["ha"]},
-    {"term": "ひ", "dictionary": "hiragana", "readings": ["hi"]},
-    {"term": "ふ", "dictionary": "hiragana", "readings": ["fu", "hu"]},
-    {"term": "へ", "dictionary": "hiragana", "readings": ["he"]},
-    {"term": "ほ", "dictionary": "hiragana", "readings": ["ho"]},
-
-    {"term": "ま", "dictionary": "hiragana", "readings": ["ma"]},
-    {"term": "み", "dictionary": "hiragana", "readings": ["mi"]},
-    {"term": "む", "dictionary": "hiragana", "readings": ["mu"]},
-    {"term": "め", "dictionary": "hiragana", "readings": ["me"]},
-    {"term": "も", "dictionary": "hiragana", "readings": ["mo"]},
-
-    {"term": "や", "dictionary": "hiragana", "readings": ["ya"]},
-    {"term": "ゆ", "dictionary": "hiragana", "readings": ["yu"]},
-    {"term": "よ", "dictionary": "hiragana", "readings": ["yo"]},
-
-    {"term": "ら", "dictionary": "hiragana", "readings": ["ra"]},
-    {"term": "り", "dictionary": "hiragana", "readings": ["ri"]},
-    {"term": "る", "dictionary": "hiragana", "readings": ["ru"]},
-    {"term": "れ", "dictionary": "hiragana", "readings": ["re"]},
-    {"term": "ろ", "dictionary": "hiragana", "readings": ["ro"]},
-
-    {"term": "わ", "dictionary": "hiragana", "readings": ["wa"]},
-    {"term": "ゐ", "dictionary": "hiragana", "readings": ["wi"]},
-    {"term": "ゑ", "dictionary": "hiragana", "readings": ["we"]},
-    {"term": "を", "dictionary": "hiragana", "readings": ["wo"]},
-    {"term": "ん", "dictionary": "hiragana", "readings": ["n"]},
-
-
-
-    {"term": "きゃ", "dictionary": "hiragana", "readings": ["kya"]},
-    {"term": "きゅ", "dictionary": "hiragana", "readings": ["kyu"]},
-    {"term": "きょ", "dictionary": "hiragana", "readings": ["kyo"]},
-
-    {"term": "しゃ", "dictionary": "hiragana", "readings": ["sha"]},
-    {"term": "しゅ", "dictionary": "hiragana", "readings": ["shu"]},
-    {"term": "しょ", "dictionary": "hiragana", "readings": ["sho"]},
-
-    {"term": "ちゃ", "dictionary": "hiragana", "readings": ["cha"]},
-    {"term": "ちゅ", "dictionary": "hiragana", "readings": ["chu"]},
-    {"term": "ちょ", "dictionary": "hiragana", "readings": ["cho"]},
-
-    {"term": "にゃ", "dictionary": "hiragana", "readings": ["nya"]},
-    {"term": "にゅ", "dictionary": "hiragana", "readings": ["nyu"]},
-    {"term": "にょ", "dictionary": "hiragana", "readings": ["nyo"]},
-
-    {"term": "ひゃ", "dictionary": "hiragana", "readings": ["hya"]},
-    {"term": "ひゅ", "dictionary": "hiragana", "readings": ["hyu"]},
-    {"term": "ひょ", "dictionary": "hiragana", "readings": ["hyo"]},
-
-    {"term": "みゃ", "dictionary": "hiragana", "readings": ["mya"]},
-    {"term": "みゅ", "dictionary": "hiragana", "readings": ["myu"]},
-    {"term": "みょ", "dictionary": "hiragana", "readings": ["myo"]},
-
-    {"term": "りゃ", "dictionary": "hiragana", "readings": ["rya"]},
-    {"term": "りゅ", "dictionary": "hiragana", "readings": ["ryu"]},
-    {"term": "りょ", "dictionary": "hiragana", "readings": ["ryo"]},
-
-
-
-    {"term": "が", "dictionary": "hiragana", "readings": ["ga"]},
-    {"term": "ぎ", "dictionary": "hiragana", "readings": ["gi"]},
-    {"term": "ぐ", "dictionary": "hiragana", "readings": ["gu"]},
-    {"term": "げ", "dictionary": "hiragana", "readings": ["ge"]},
-    {"term": "ご", "dictionary": "hiragana", "readings": ["go"]},
-
-    {"term": "ざ", "dictionary": "hiragana", "readings": ["za"]},
-    {"term": "じ", "dictionary": "hiragana", "readings": ["ji"]},
-    {"term": "ず", "dictionary": "hiragana", "readings": ["zu"]},
-    {"term": "ぜ", "dictionary": "hiragana", "readings": ["ze"]},
-    {"term": "ぞ", "dictionary": "hiragana", "readings": ["zo"]},
-
-    {"term": "だ", "dictionary": "hiragana", "readings": ["da"]},
-    {"term": "ぢ", "dictionary": "hiragana", "readings": ["ji"]},
-    {"term": "づ", "dictionary": "hiragana", "readings": ["zu"]},
-    {"term": "で", "dictionary": "hiragana", "readings": ["de"]},
-    {"term": "ど", "dictionary": "hiragana", "readings": ["do"]},
-
-    {"term": "ば", "dictionary": "hiragana", "readings": ["ba"]},
-    {"term": "び", "dictionary": "hiragana", "readings": ["bi"]},
-    {"term": "ぶ", "dictionary": "hiragana", "readings": ["bu"]},
-    {"term": "べ", "dictionary": "hiragana", "readings": ["be"]},
-    {"term": "ぼ", "dictionary": "hiragana", "readings": ["bo"]},
-
-    {"term": "ぱ", "dictionary": "hiragana", "readings": ["pa"]},
-    {"term": "ぴ", "dictionary": "hiragana", "readings": ["pi"]},
-    {"term": "ぷ", "dictionary": "hiragana", "readings": ["pu"]},
-    {"term": "ぺ", "dictionary": "hiragana", "readings": ["pe"]},
-    {"term": "ぽ", "dictionary": "hiragana", "readings": ["po"]},
-
-
-
-    {"term": "ぎゃ", "dictionary": "hiragana", "readings": ["gya"]},
-    {"term": "ぎゅ", "dictionary": "hiragana", "readings": ["gyu"]},
-    {"term": "ぎょ", "dictionary": "hiragana", "readings": ["gyo"]},
-
-    {"term": "じゃ", "dictionary": "hiragana", "readings": ["ja"]},
-    {"term": "じゅ", "dictionary": "hiragana", "readings": ["ju"]},
-    {"term": "じょ", "dictionary": "hiragana", "readings": ["jo"]},
-
-    {"term": "ぢゃ", "dictionary": "hiragana", "readings": ["ja"]},
-    {"term": "ぢゅ", "dictionary": "hiragana", "readings": ["ju"]},
-    {"term": "ぢょ", "dictionary": "hiragana", "readings": ["jo"]},
-
-    {"term": "びゃ", "dictionary": "hiragana", "readings": ["bya"]},
-    {"term": "びゅ", "dictionary": "hiragana", "readings": ["byu"]},
-    {"term": "びょ", "dictionary": "hiragana", "readings": ["byo"]},
-
-    {"term": "ぴゃ", "dictionary": "hiragana", "readings": ["pya"]},
-    {"term": "ぴゅ", "dictionary": "hiragana", "readings": ["pyu"]},
-    {"term": "ぴょ", "dictionary": "hiragana", "readings": ["pyo"]}
-];
-
-
-var hiragana_simple = [
     {"term": "&#12354;", "dictionary": "hiragana", "readings": ["a"]},
     {"term": "&#12356;", "dictionary": "hiragana", "readings": ["i"]},
     {"term": "&#12358;", "dictionary": "hiragana", "readings": ["u"]},
@@ -305,12 +143,177 @@ var hiragana_simple = [
     {"term": "&#12404;&#12423;", "dictionary": "hiragana", "readings": ["pyo"]}
 ];
 
+var katakana = [
+    {"term": "&#12450;", "dictionary": "katakana", "readings": ["a"]},
+    {"term": "&#12452;", "dictionary": "katakana", "readings": ["i"]},
+    {"term": "&#12454;", "dictionary": "katakana", "readings": ["u"]},
+    {"term": "&#12456;", "dictionary": "katakana", "readings": ["e"]},
+    {"term": "&#12458;", "dictionary": "katakana", "readings": ["o"]},
 
-console.log(hiragana[0]['readings']);
+    {"term": "&#12459;", "dictionary": "katakana", "readings": ["ka"]},
+    {"term": "&#12461;", "dictionary": "katakana", "readings": ["ki"]},
+    {"term": "&#12463;", "dictionary": "katakana", "readings": ["ku"]},
+    {"term": "&#12465;", "dictionary": "katakana", "readings": ["ke"]},
+    {"term": "&#12467;", "dictionary": "katakana", "readings": ["ko"]},
 
-var quiz = hiragana_simple;
+    {"term": "&#12469;", "dictionary": "katakana", "readings": ["sa"]},
+    {"term": "&#12471;", "dictionary": "katakana", "readings": ["shi", "si"]},
+    {"term": "&#12473;", "dictionary": "katakana", "readings": ["su"]},
+    {"term": "&#12475;", "dictionary": "katakana", "readings": ["se"]},
+    {"term": "&#12477;", "dictionary": "katakana", "readings": ["so"]},
+
+    {"term": "&#12479;", "dictionary": "katakana", "readings": ["ta"]},
+    {"term": "&#12481;", "dictionary": "katakana", "readings": ["chi"]},
+    {"term": "&#12484;", "dictionary": "katakana", "readings": ["tsu"]},
+    {"term": "&#12486;", "dictionary": "katakana", "readings": ["te"]},
+    {"term": "&#12488;", "dictionary": "katakana", "readings": ["to"]},
+
+    {"term": "&#12490;", "dictionary": "katakana", "readings": ["na"]},
+    {"term": "&#12491;", "dictionary": "katakana", "readings": ["ni"]},
+    {"term": "&#12492;", "dictionary": "katakana", "readings": ["nu"]},
+    {"term": "&#12493;", "dictionary": "katakana", "readings": ["ne"]},
+    {"term": "&#12494;", "dictionary": "katakana", "readings": ["no"]},
+
+    {"term": "&#12495;", "dictionary": "katakana", "readings": ["ha"]},
+    {"term": "&#12498;", "dictionary": "katakana", "readings": ["hi"]},
+    {"term": "&#12501;", "dictionary": "katakana", "readings": ["fu", "hu"]},
+    {"term": "&#12504;", "dictionary": "katakana", "readings": ["he"]},
+    {"term": "&#12507;", "dictionary": "katakana", "readings": ["ho"]},
+
+    {"term": "&#12510;", "dictionary": "katakana", "readings": ["ma"]},
+    {"term": "&#12511;", "dictionary": "katakana", "readings": ["mi"]},
+    {"term": "&#12512;", "dictionary": "katakana", "readings": ["mu"]},
+    {"term": "&#12513;", "dictionary": "katakana", "readings": ["me"]},
+    {"term": "&#12514;", "dictionary": "katakana", "readings": ["mo"]},
+
+    {"term": "&#12516;", "dictionary": "katakana", "readings": ["ya"]},
+    {"term": "&#12518;", "dictionary": "katakana", "readings": ["yu"]},
+    {"term": "&#12520;", "dictionary": "katakana", "readings": ["yo"]},
+
+    {"term": "&#12521;", "dictionary": "katakana", "readings": ["ra"]},
+    {"term": "&#12522;", "dictionary": "katakana", "readings": ["ri"]},
+    {"term": "&#12523;", "dictionary": "katakana", "readings": ["ru"]},
+    {"term": "&#12524;", "dictionary": "katakana", "readings": ["re"]},
+    {"term": "&#12525;", "dictionary": "katakana", "readings": ["ro"]},
+
+    {"term": "&#12527;", "dictionary": "katakana", "readings": ["wa"]},
+    {"term": "&#12454;&#12451;", "dictionary": "katakana", "readings": ["wi"]},
+    {"term": "&#12454;&#12455;", "dictionary": "katakana", "readings": ["we"]},
+    {"term": "&#12530;", "dictionary": "katakana", "readings": ["wo"]},
+    {"term": "&#12531;", "dictionary": "katakana", "readings": ["n"]},
 
 
+
+    {"term": "&#12461;&#12515;", "dictionary": "katakana", "readings": ["kya"]},
+    {"term": "&#12461;&#12517;", "dictionary": "katakana", "readings": ["kyu"]},
+    {"term": "&#12461;&#12519;", "dictionary": "katakana", "readings": ["kyo"]},
+
+    {"term": "&#12471;&#12515;", "dictionary": "katakana", "readings": ["sha"]},
+    {"term": "&#12471;&#12517;", "dictionary": "katakana", "readings": ["shu"]},
+    {"term": "&#12471;&#12519;", "dictionary": "katakana", "readings": ["sho"]},
+
+    {"term": "&#12481;&#12515;", "dictionary": "katakana", "readings": ["cha"]},
+    {"term": "&#12481;&#12517;", "dictionary": "katakana", "readings": ["chu"]},
+    {"term": "&#12481;&#12519;", "dictionary": "katakana", "readings": ["cho"]},
+
+    {"term": "&#12491;&#12515;", "dictionary": "katakana", "readings": ["nya"]},
+    {"term": "&#12491;&#12517;", "dictionary": "katakana", "readings": ["nyu"]},
+    {"term": "&#12491;&#12519;", "dictionary": "katakana", "readings": ["nyo"]},
+
+    {"term": "&#12498;&#12515;", "dictionary": "katakana", "readings": ["hya"]},
+    {"term": "&#12498;&#12517;", "dictionary": "katakana", "readings": ["hyu"]},
+    {"term": "&#12498;&#12519;", "dictionary": "katakana", "readings": ["hyo"]},
+
+    {"term": "&#12511;&#12515;", "dictionary": "katakana", "readings": ["mya"]},
+    {"term": "&#12511;&#12517;", "dictionary": "katakana", "readings": ["myu"]},
+    {"term": "&#12511;&#12519;", "dictionary": "katakana", "readings": ["myo"]},
+
+    {"term": "&#12522;&#12515;", "dictionary": "katakana", "readings": ["rya"]},
+    {"term": "&#12522;&#12517;", "dictionary": "katakana", "readings": ["ryu"]},
+    {"term": "&#12522;&#12519;", "dictionary": "katakana", "readings": ["ryo"]},
+
+
+
+    {"term": "&#12460;", "dictionary": "katakana", "readings": ["ga"]},
+    {"term": "&#12462;", "dictionary": "katakana", "readings": ["gi"]},
+    {"term": "&#12464;", "dictionary": "katakana", "readings": ["gu"]},
+    {"term": "&#12466;", "dictionary": "katakana", "readings": ["ge"]},
+    {"term": "&#12468;", "dictionary": "katakana", "readings": ["go"]},
+
+    {"term": "&#12470;", "dictionary": "katakana", "readings": ["za"]},
+    {"term": "&#12472;", "dictionary": "katakana", "readings": ["ji"]},
+    {"term": "&#12474;", "dictionary": "katakana", "readings": ["zu"]},
+    {"term": "&#12476;", "dictionary": "katakana", "readings": ["ze"]},
+    {"term": "&#12478;", "dictionary": "katakana", "readings": ["zo"]},
+
+    {"term": "&#12480;", "dictionary": "katakana", "readings": ["da"]},
+    {"term": "&#12482;", "dictionary": "katakana", "readings": ["ji"]},
+    {"term": "&#12485;", "dictionary": "katakana", "readings": ["zu"]},
+    {"term": "&#12487;", "dictionary": "katakana", "readings": ["de"]},
+    {"term": "&#12489;", "dictionary": "katakana", "readings": ["do"]},
+
+    {"term": "&#12496;", "dictionary": "katakana", "readings": ["ba"]},
+    {"term": "&#12499;", "dictionary": "katakana", "readings": ["bi"]},
+    {"term": "&#12502;", "dictionary": "katakana", "readings": ["bu"]},
+    {"term": "&#12505;", "dictionary": "katakana", "readings": ["be"]},
+    {"term": "&#12508;", "dictionary": "katakana", "readings": ["bo"]},
+
+    {"term": "&#12497;", "dictionary": "katakana", "readings": ["pa"]},
+    {"term": "&#12500;", "dictionary": "katakana", "readings": ["pi"]},
+    {"term": "&#12503;", "dictionary": "katakana", "readings": ["pu"]},
+    {"term": "&#12506;", "dictionary": "katakana", "readings": ["pe"]},
+    {"term": "&#12509;", "dictionary": "katakana", "readings": ["po"]},
+
+
+
+    {"term": "&#12462;&#12515;", "dictionary": "katakana", "readings": ["gya"]},
+    {"term": "&#12462;&#12517;", "dictionary": "katakana", "readings": ["gyu"]},
+    {"term": "&#12462;&#12519;", "dictionary": "katakana", "readings": ["gyo"]},
+
+    {"term": "&#12472;&#12515;", "dictionary": "katakana", "readings": ["ja"]},
+    {"term": "&#12472;&#12517;", "dictionary": "katakana", "readings": ["ju"]},
+    {"term": "&#12472;&#12519;", "dictionary": "katakana", "readings": ["jo"]},
+
+    {"term": "&#12499;&#12515;", "dictionary": "katakana", "readings": ["bya"]},
+    {"term": "&#12499;&#12517;", "dictionary": "katakana", "readings": ["byu"]},
+    {"term": "&#12499;&#12519;", "dictionary": "katakana", "readings": ["byo"]},
+
+    {"term": "&#12500;&#12515;", "dictionary": "katakana", "readings": ["pya"]},
+    {"term": "&#12500;&#12517;", "dictionary": "katakana", "readings": ["pyu"]},
+    {"term": "&#12500;&#12519;", "dictionary": "katakana", "readings": ["pyo"]}
+];
+
+
+// ========================
+// Alphabet selection
+
+var quiz = [];
+
+var choice = $.cookie('alphabet');
+
+if (choice = "hk") {
+    quiz = hiragana.concat(katakana);
+} else if (choice = "k") {
+    quiz = katakana;
+} else {
+    quiz = hiragana;
+}
+
+function save_prefs(choice){
+    // choices: h, k or hk
+    $.cookie('alphabet', choice);
+}
+
+function load_prefs(choice){
+    // set quiz
+    // set checkboxes
+}
+
+
+
+
+// ========================
+// Generate question
 
 var num = Math.floor( Math.random()*quiz.length );
 var chr = quiz[num];
@@ -345,6 +348,10 @@ for (var i=0; i<choices.length; i++){
     console.log( choice + i );
 };
 
+
+// ========================
+// Interface
+
 $('.correct').click(function(){
    // alert("Correct!"); 
    // $('.incorrect').fadeTo(500,0.1);
@@ -361,4 +368,39 @@ $('.incorrect').click(function(){
 $('.next').click(function(){
     location.reload();
 });
+
+
+Object.prototype.getKeyByValue = function( value ) {
+    for( var prop in this ) {
+        if( this.hasOwnProperty( prop ) ) {
+             if( this[ prop ] === value )
+                 return prop;
+        }
+    }
+}
+
+var a_a = {
+};
+
+
+var update_checked = function() {
+    var chkbx = this.children;
+    if (chkbx.checked) {
+        a_a[ $(chkbx).val() ] = true;
+    } else {
+        a_a[ $(chkbx).val() ] = false;
+    }
+    console.log(a_a);
+
+    if ((a_a["h"] = true) && (a_a["k"] = true)) {
+        save_prefs("hk");
+    } else if (a_a["k"] = true) {
+        save_prefs("k");
+    } else {
+        save_prefs("h");
+    }
+};
+ 
+$( "label" ).change( update_checked );
+
 
